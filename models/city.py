8dbd437e2535fc5496e4ec503757053972d7e4dd
Shalomtash <99330267+shalomtash@users.noreply.github.com>
2022-08-15 22:08:15 +0300

    Update city.py

--- models/city.py
+++ models/city.py
@@ -1,11 +1,19 @@
 #!/usr/bin/python3
-"""
-Defines city
-"""
-from models.base_model import BaseModel
+""" City Module for HBNB project """
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, String, ForeignKey
+from models import storage_type
+from sqlalchemy.orm import relationship
 
 
-class City(BaseModel):
-    """defines city to look for"""
-    state_id = ""
-    name = ""
+class City(BaseModel, Base):
+    """ The city class, contains state ID and name """
+
+    if storage_type == 'db':
+        __tablename__ = "cities"
+        state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
+        name = Column(String(128), nullable=False)
+        places = relationship('Place', backref='city', cascade="all")
+    else:
+        state_id = ""
+        name = ""

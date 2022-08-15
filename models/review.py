e1ffe7ff1b9ce96762f1ebacb89d4596a1966c32
Shalomtash <99330267+shalomtash@users.noreply.github.com>
2022-08-15 22:10:20 +0300

    Update review.py

--- models/review.py
+++ models/review.py
@@ -1,16 +1,22 @@
 #!/usr/bin/python3
-"""Defines the Review class."""
-from models.base_model import BaseModel
+""" Review module for the HBNB project """
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, String, ForeignKey
+from models import storage_type
+from sqlalchemy.orm import relationship
 
 
-class Review(BaseModel):
-    """Represent a review.
-    Attributes:
-        place_id (str): The Place id.
-        user_id (str): The User id.
-        text (str): The text of the review.
-    """
+class Review(BaseModel, Base):
+    """ Review classto store review information """
 
-    place_id = ""
-    user_id = ""
-    text = ""
+    if storage_type == 'db':
+        __tablename__ = 'reviews'
+
+        place_id = Column(String(60), ForeignKey("places.id"), nullable=False)
+        user_id = Column(String(60), ForeignKey("users.id"), nullable=False)
+        text = Column(String(1024), nullable=False)
+
+    else:
+        place_id = ""
+        user_id = ""
+        text = ""
